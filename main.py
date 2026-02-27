c5a1b07c50a61196da7933f8c8f90f60536d0a35
Toby Ang <58008827+tang0226@users.noreply.github.com>
2026-02-27 10:59:01 -0800

    Add network classes and test basic agent operation within environment

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import torch
+from torch import nn
 import gymnasium as gym
 import pygame
 import numpy as np
 
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else 'cpu'
+print(f'Using {device} device')
+
+###################
+### ENVIRONMENT ###
+###################
 class SlipperyEnv(gym.Env):
   def __init__(
       self,
       size: int = 500,
       agent_size: int = 20,
       friction: float = 0.95,
-      accel_coeff = 0.0001,
+      accel = 0.05,
       min_target_dist_coeff: float = 0.4,
-      max_steps: int = 1000,
+      goal_reward: float = 20,
+      time_penalty: float = -1/60, # lose 1 reward for every second
+      edge_penalty: float = -20,
+      max_steps: int = 1800,
       render_mode = 'human',
   ):
     self.metadata = {'render_modes': ['human', 'rgb_array'], 'render_fps': 60}
@@ -19,14 +29,20 @@ class SlipperyEnv(gym.Env):
     self.size = size                                    # Size of space in pixels
     self.agent_size = agent_size                        # Size of agent's circular collision shape
     self.friction = friction                            # Coefficient of friction
-    self.accel_coeff = accel_coeff                      # Strength of movement actions, relative to the size of the space
+    self.accel = accel                                  # Acceleration of movement, measured in px/frame^2
     self.min_target_dist_coeff = min_target_dist_coeff  # Relates environment size to the min distance for agent to travel
+    self.goal_reward = goal_reward                      # Reward for reaching goal
+    self.time_penalty = time_penalty                    # Reward lost with each frame
+    self.edge_penalty = edge_penalty                    # Reward lost for hitting edge
     self.max_steps = max_steps                          # max number of steps before an episode is truncated; passed to the TimeLimit wrapper
+
+    assert self.friction < 1 and self.friction > 0
 
     if (self.min_target_dist_coeff >= 0.5):
       raise ValueError('min_target_dist_coeff must be less than 0.5')
 
     self.min_target_dist = self.size * self.min_target_dist_coeff
+    self.max_lateral_speed = self.accel / (1 - self.friction)
 
     self._agent_position = np.array([0, 0], dtype=float)
     self._target_position = np.array([0, 0], dtype=float)
@@ -41,7 +57,6 @@ class SlipperyEnv(gym.Env):
 
     self.action_space = gym.spaces.Discrete(4)
 
-    self.accel = self.size * self.accel_coeff
     self._action_to_dv = {
       0: np.array([0, self.accel]),
       1: np.array([self.accel, self.accel]),
@@ -52,6 +67,8 @@ class SlipperyEnv(gym.Env):
       6: np.array([-self.accel, 0]),
       7: np.array([-self.accel, self.accel]),
     }
+
+    self._latest_action = None
 
     # ensure a valid render_mode value is being passed
     assert render_mode is None or render_mode in self.metadata['render_modes']
@@ -69,8 +86,24 @@ class SlipperyEnv(gym.Env):
     Returns:
       dict: observation of current environment (agent and target)
     """
-    return {'agent': self._agent_position, 'target': self._target_position}
+    return {'agent': self._agent_position, 'target': self._target_position, 'v': self._agent_v}
   
+  def normalize_obs(self, obs):
+    """Normalizes values of obs based on environment size and max possible speed
+    Args:
+      obs: an observation dict returned by .reset() or .step()
+    Returns:
+      list[float]: a list of normalized observation values
+    """
+    return [
+      obs['agent'][0] / self.size,
+      obs['agent'][1] / self.size,
+      obs['target'][0] / self.size,
+      obs['target'][1] / self.size,
+      obs['v'][0] / self.max_lateral_speed,
+      obs['v'][1] / self.max_lateral_speed,
+    ]
+
   def _get_info(self):
     return None
 
@@ -141,9 +174,12 @@ class SlipperyEnv(gym.Env):
     Returns:
       tuple: (observation, reward, terminated, truncated, info)
     """
+    self._latest_action = action
+
     # accelerate the agent according to the action
     dv = self._action_to_dv[action]
     self._agent_v += dv
+    self._agent_v *= self.friction
 
     # update the agent's position based on its velocity
     self._agent_position += self._agent_v
@@ -151,10 +187,10 @@ class SlipperyEnv(gym.Env):
     # reward
     reward = 0
     if self._get_dist_to_target() < self.agent_size / 2:
-      reward += 1
+      reward += self.goal_reward
       self._reset_target_position()
     else:
-      reward += -0.01
+      reward += self.time_penalty
     
     pos = self._agent_position
     if (
@@ -164,7 +200,7 @@ class SlipperyEnv(gym.Env):
       pos[1] > self.size
     ):
       # heavy penalty for "falling off the edge"
-      reward += -3
+      reward += self.edge_penalty
       # respawn agent
       self._respawn_agent()
     
@@ -198,7 +234,7 @@ class SlipperyEnv(gym.Env):
       canvas,
       (0, 170, 0),
       self._target_position,
-      10
+      5
     )
     # agent
     pygame.draw.circle(
@@ -222,18 +258,74 @@ class SlipperyEnv(gym.Env):
   def render(self):
     self._render_canvas()
 
+#####################
+### AGENT NETWORK ###
+#####################
+class SlipperyAgentNN(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.linear_relu_stack = nn.Sequential(
+      nn.Linear(6, 64),
+      nn.ReLU(),
+      nn.Linear(64, 64),
+      nn.ReLU(),
+      nn.Linear(64, 8),
+    )
+
+  def forward(self, x):
+    logits = self.linear_relu_stack(x)
+    return logits
+
+#################################
+### REWARD ESTIMATION NETWORK ###
+#################################
+class SlipperyRewardEstNN(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.linear_relu_stack = nn.Sequential(
+      nn.Linear(6, 64),
+      nn.ReLU(),
+      nn.Linear(64, 64),
+      nn.ReLU(),
+      nn.Linear(64, 1),
+    )
+
+  def forward(self, x):
+    logits = self.linear_relu_stack(x)
+    return logits
+
+
+
 env = SlipperyEnv(
-  agent_size=50,
+  size=500,
+  agent_size=30,
+  friction=0.995,
   min_target_dist_coeff=0.2,
 )
 
 env = gym.wrappers.TimeLimit(env, env.max_steps)
+unwrapped = env.unwrapped
 
-env.reset()
+agent_network = SlipperyAgentNN()
+agent_network.to(device)
 
-for i in range(3):
+est_network = SlipperyRewardEstNN()
+est_network.to(device)
+
+observation, info = env.reset()
+
+for episode in range(5):
+  total_reward = 0
+  observation, info = env.reset()
   while True:
-    observation, reward, terminated, truncated, info = env.step(np.random.randint(0, 8))
+    normalized = torch.Tensor(unwrapped.normalize_obs(observation))
+    normalized = normalized.to(device) # move to gpu
+
+    # get action from agent network
+    action = torch.argmax(agent_network.forward(normalized)).item()
+    observation, reward, terminated, truncated, info = env.step(action)
+    total_reward += reward
     env.render()
     if terminated or truncated:
       break
+  print(episode, total_reward)
